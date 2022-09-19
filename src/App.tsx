8529db42350d842984f88918617368ee0c4512c0
Krutarth Darji <krutarth.darji@tigeranalytics.local>
2022-09-19 17:36:39 +0530

    useContext basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Button from "./components/Button";
 import Container from "./components/Container";
+import Box from "./components/context/Box";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
 import Greet from "./components/Greet";
 import Heading from "./components/Heading";
 import Input from "./components/Input";
 import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import Counter from "./components/state/Counter";
 import Status from "./components/Status";
 
 function App() {
@@ -47,6 +50,10 @@ function App() {
           whiteSpace: "nowrap",
         }}
       />
+      <Counter />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
